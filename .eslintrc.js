c203e8df3b99abf99c23ea6b2486f31e93a05916
nopol10 <bobnewbie@gmail.com>
2020-12-30 15:04:52 +0800

    chore: add husky pre-commit hook that runs eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
+    "prettier",
   ],
   rules: {
     "@typescript-eslint/ban-ts-comment": "off",
