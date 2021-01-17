a93183f0d1a3ea837173998265e3fe9cf8b152e5
nopol10 <bobnewbie@gmail.com>
2021-01-17 10:50:22 +0800

    build: add zip-source script

---
+++ zip-source.js
@@ -0,0 +1,55 @@
+const AdmZip = require("adm-zip");
+const fs = require("fs");
+
+const date = new Date();
+
+const zip = new AdmZip();
+
+const pad = (inString, length, padCharacter) => {
+  const stringLength = inString.toString().length;
+  if (stringLength < length) {
+    for (let i = 0; i < length - stringLength; i++) {
+      inString = padCharacter + inString;
+    }
+  }
+  return inString;
+};
+
+zip.addLocalFolder("../nekocap", "", (filename) => {
+  const excludedFiles = [
+    ".env",
+    "dist/",
+    "docker/",
+    ".git",
+    ".vscode",
+    "node_modules",
+    "server-fonts",
+    "webdist",
+    ".eslintcache",
+    "Dockerfile",
+    ".zip",
+    "package-backup.json",
+    "zip-source.js",
+  ];
+  for (let i = 0; i < excludedFiles.length; i++) {
+    if (filename.indexOf(excludedFiles[i]) >= 0) {
+      return false;
+    }
+  }
+  return true;
+});
+const filename = `./nekocap-source-${date.getFullYear()}-${pad(
+  date.getMonth() + 1,
+  2,
+  "0"
+)}-${pad(date.getDate(), 2, "0")}-${pad(date.getHours(), 2, "0")}-${pad(
+  date.getMinutes(),
+  2,
+  "0"
+)}.zip`;
+
+if (fs.existsSync(filename)) {
+  fs.unlinkSync(filename);
+}
+
+zip.writeZip(filename);
