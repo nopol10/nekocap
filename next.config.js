c32295984224d457f36cdeb1fedbab35d0b98e89
nopol10 <bobnewbie@gmail.com>
2021-05-16 15:43:35 +0800

    feat: update packages to be able to generate nextjs pages and the extension correctly

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
 const { i18n } = require("./next-i18next.config");
 const withImages = require("next-images");
 const withAntdLess = require("next-plugin-antd-less");
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
 module.exports = withAntdLess(
   withImages({
     i18n,
-    lessLoaderOptions: {
-      javascriptEnabled: true,
+    cssLoaderOptions: {
+      modules: true,
     },
-    lessVarsFilePath: "./src/theme.less",
+    modifyVars: { "@primary-color": "#ffa62b" },
+    webpack: (config, { dev }) => {
+      const envFile = dotenv.config(
+        dev ? undefined : { path: "./.env.prod" }
+      ).parsed;
+      const envKeys = Object.keys(envFile).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(envFile[next]);
+        return prev;
+      }, {});
+      config.plugins = [...config.plugins, new webpack.DefinePlugin(envKeys)];
+      return config;
+    },
   })
 );
