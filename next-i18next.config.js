9017feba3402d2b9af27becd3e789ab0220ca54a
nopol10 <bobnewbie@gmail.com>
2021-05-16 23:00:54 +0800

    fix: fix locales not working in vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
+const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
+    localePath: path.resolve("./public/locales"),
   },
 };
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const { i18n } = require("./next-i18next.config");
 const withImages = require("next-images");
 const withAntdLess = require("next-plugin-antd-less");
-const dotenv = require("dotenv");
-const webpack = require("webpack");
 
 module.exports = withAntdLess(
   withImages({
@@ -11,19 +9,5 @@ module.exports = withAntdLess(
       modules: true,
     },
     modifyVars: { "@primary-color": "#ffa62b" },
-    // webpack: (config, { dev }) => {
-    //   const envFileConfig = dotenv.config(
-    //     dev ? undefined : { path: "./.env.prod" }
-    //   );
-    //   if (envFileConfig) {
-    //     const envFile = envFileConfig.parsed;
-    //     const envKeys = Object.keys(envFile).reduce((prev, next) => {
-    //       prev[`process.env.${next}`] = JSON.stringify(envFile[next]);
-    //       return prev;
-    //     }, {});
-    //     config.plugins = [...config.plugins, new webpack.DefinePlugin(envKeys)];
-    //   }
-    //   return config;
-    // },
   })
 );
