516a84c99f94a63cb8772da36e198fe9403ef0f9
nopol10 <bobnewbie@gmail.com>
2022-02-01 16:30:57 +0800

    fix: fix firebase not being initialized in the popup page

--- src/extension/popup/index.tsx
+++ src/extension/popup/index.tsx
@@ -6,27 +6,24 @@ import LoginRoutes from "./feature/login/containers/routes";
 import { ChromeMessage, ChromeMessageType } from "@/common/types";
 import { appHistory } from "./common/store";
 import "../../ant.less";
-import {
-  requestBackgroundPageVariable,
-  syncWindowVarsToPopup,
-} from "@/common/chrome-utils";
+import { syncWindowVarsToPopup } from "@/common/chrome-utils";
 import "@/extension/popup/common/styles/index.scss";
 import "@/extension/background/common/provider";
 import { PopupProvider } from "../common/popup-context";
 import { storeInitPromise } from "@/extension/background/common/store";
+import { initFirebase } from "../background/firebase";
 window.isPopupScript = true;
 
-chrome.runtime.onMessage.addListener(
-  (request: ChromeMessage, sender, sendResponse) => {
-    if (request.type === ChromeMessageType.Route) {
-      appHistory.push(request.payload);
-    }
+initFirebase();
+
+chrome.runtime.onMessage.addListener((request: ChromeMessage) => {
+  if (request.type === ChromeMessageType.Route) {
+    appHistory.push(request.payload);
   }
-);
+});
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
   storeInitPromise.then(async ({ store }) => {
-    await requestBackgroundPageVariable(["backendProvider"]);
     await syncWindowVarsToPopup(tab[0].id);
     ReactDOM.render(
       <Provider store={store}>
