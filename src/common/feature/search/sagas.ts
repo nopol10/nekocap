55fb568be871eaabad0552d9b90107a940580eb8
nopol10 <bobnewbie@gmail.com>
2022-03-14 23:52:01 +0800

    feat(search): add loadSearchResultVideoCaptions saga

--- src/common/feature/search/sagas.ts
+++ src/common/feature/search/sagas.ts
@@ -11,19 +11,21 @@ import { PayloadAction } from "@reduxjs/toolkit";
 import { getLimitOffsetFromPagination } from "@/common/utils";
 import { safe } from "@/common/redux-utils";
 import {
+  loadSearchResultVideoCaptions,
   search,
   searchFromBasicBar,
   setSearchNoMoreResults as setNoMoreSearchResults,
   setSearchResults,
+  setSearchResultVideoCaptions,
 } from "./actions";
 import {
+  LoadSearchResultVideoCaptions,
   SearchParams,
   SetVideoSearchResults,
   VideoSearchResults,
 } from "./types";
 import { routeNames } from "@/web/feature/route-types";
-import { VideoFields } from "../video/types";
-import { webHistory } from "@/web/feature/web-history";
+import { LoadCaptionsResult, VideoFields } from "../video/types";
 import { searchSelector } from "./selectors";
 import { populateVideoDetails } from "./api";
 import { Locator } from "@/common/locator/locator";
@@ -36,11 +38,15 @@ function* searchRequestSaga(action: PayloadAction<SearchParams>) {
   if (!originalHasMoreResults && append) {
     return;
   }
-  const { status, error, videos, hasMoreResults }: VideoSearchResults =
-    yield call([Locator.provider(), "search"], {
-      ...getLimitOffsetFromPagination(pageSize, pageNumber),
-      ...rest,
-    });
+  const {
+    status,
+    error,
+    videos,
+    hasMoreResults,
+  }: VideoSearchResults = yield call([Locator.provider(), "search"], {
+    ...getLimitOffsetFromPagination(pageSize, pageNumber),
+    ...rest,
+  });
   if (status === "error") {
     throw new Error(error);
   }
@@ -70,6 +76,20 @@ function* searchSuccessSaga({
   yield put(setSearchResults(videos));
 }
 
+function* loadSearchResultVideoCaptionsRequestSaga({
+  payload,
+}: PayloadAction<LoadSearchResultVideoCaptions>) {
+  const { videoId, videoSource } = payload;
+  const result: LoadCaptionsResult[] = yield call(
+    [Locator.provider(), "loadCaptions"],
+    {
+      videoId,
+      videoSource,
+    }
+  );
+  yield put(setSearchResultVideoCaptions({ captions: result }));
+}
+
 function* searchFromBasicBarSaga({ payload: title }: PayloadAction<string>) {
   window.location.href = routeNames.search.replace(":title?", title);
   yield;
@@ -78,6 +98,13 @@ function* searchFromBasicBarSaga({ payload: title }: PayloadAction<string>) {
 function* searchSaga() {
   yield throttle(1000, search.REQUEST, search.requestSaga(searchRequestSaga));
   yield takeLatest(search.SUCCESS, safe(searchSuccessSaga));
+
+  yield takeLatest(
+    loadSearchResultVideoCaptions.REQUEST,
+    loadSearchResultVideoCaptions.requestSaga(
+      loadSearchResultVideoCaptionsRequestSaga
+    )
+  );
 
   yield takeLatest(searchFromBasicBar.type, safe(searchFromBasicBarSaga));
 }
