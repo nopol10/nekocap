8821b18ddf10994fc45cd42a9208cdf0d40327bd
nopol10 <bobnewbie@gmail.com>
2022-07-17 20:39:32 +0800

    refactor: modify kofi script loading method

--- src/web/feature/common/containers/kofi-widget.tsx
+++ src/web/feature/common/containers/kofi-widget.tsx
@@ -1,8 +1,7 @@
 import { colors } from "@/common/colors";
-import { waitUntil } from "@/common/utils";
 import { useTranslation } from "next-i18next";
-import Head from "next/head";
-import React, { ReactElement, useEffect } from "react";
+import Script from "next/script";
+import React, { ReactElement, useCallback } from "react";
 import { createGlobalStyle } from "styled-components";
 
 const KofiCSSOverride = createGlobalStyle`
@@ -24,27 +23,24 @@ const KofiCSSOverride = createGlobalStyle`
 `;
 export const KofiWidget = (): ReactElement => {
   const { t } = useTranslation("common");
-  useEffect(() => {
-    (async () => {
-      await waitUntil(() => typeof window["kofiWidgetOverlay"] !== undefined);
-      window.kofiWidgetOverlay?.draw("nopol10", {
-        type: "floating-chat",
-        "floating-chat.donateButton.text": t("common.kofiSupportMe"),
-        "floating-chat.donateButton.background-color": colors.base,
-        "floating-chat.donateButton.text-color": "#fff",
-        "floating-chat.core.position.bottom-left":
-          "position: fixed; bottom: 50px; right: 10px; width: 160px; height: 65px;",
-      });
-    })();
+  const handleOnLoad = useCallback(() => {
+    window.kofiWidgetOverlay?.draw("nopol10", {
+      type: "floating-chat",
+      "floating-chat.donateButton.text": t("common.kofiSupportMe"),
+      "floating-chat.donateButton.background-color": colors.base,
+      "floating-chat.donateButton.text-color": "#fff",
+      "floating-chat.core.position.bottom-left":
+        "position: fixed; bottom: 50px; right: 10px; width: 160px; height: 65px;",
+    });
   }, []);
   return (
     <>
-      <Head>
-        <script
-          defer
-          src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
-        ></script>
-      </Head>
+      <Script
+        defer
+        strategy="lazyOnload"
+        src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
+        onLoad={handleOnLoad}
+      ></Script>
       <KofiCSSOverride />
     </>
   );
