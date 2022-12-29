afcb25f992b9e24a281d7d0f84840884ed7a1b0d
nopol10 <bobnewbie@gmail.com>
2022-12-29 09:37:54 +0800

    chore: update next to 12.3.4

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
-  const { pathname } = request.nextUrl;
   const url = request.nextUrl.clone();
-  if (pathname == "/browse") {
-    url.pathname = "/browse/1";
-    return NextResponse.redirect(url);
-  }
-  return NextResponse.next();
+  url.pathname = "/browse/1";
+  return NextResponse.redirect(url);
 }
+
+export const config = {
+  matcher: ["/browse"],
+};
