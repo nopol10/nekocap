c48adbd9896ac3a541431afaca0a4755882e037e
nopol10 <bobnewbie@gmail.com>
2023-03-21 21:38:38 +0800

    fix: fix web profile page null errors

--- src/web/feature/profile/admin-utils.ts
+++ src/web/feature/profile/admin-utils.ts
@@ -8,54 +8,54 @@ import { Dispatch } from "redux";
 import { message } from "antd";
 import { i18n } from "next-i18next";
 
-export const handleAssignReviewerManager = (
-  captionerId: string,
-  dispatch: Dispatch<any>
-) => () => {
-  dispatch(assignReviewerManager.request(captionerId))
-    .then(() => {
-      message.success(i18n.t("profile.roleAssignmentSuccess"));
-    })
-    .catch((error) => {
-      message.error(i18n.t("profile.roleAssignmentFailure", { error: error }));
-    });
-};
+export const handleAssignReviewerManager =
+  (captionerId: string, dispatch: Dispatch<any>) => () => {
+    dispatch(assignReviewerManager.request(captionerId))
+      .then(() => {
+        message.success(i18n?.t("profile.roleAssignmentSuccess"));
+      })
+      .catch((error) => {
+        message.error(
+          i18n?.t("profile.roleAssignmentFailure", { error: error })
+        );
+      });
+  };
 
-export const handleAssignReviewer = (
-  captionerId: string,
-  dispatch: Dispatch<any>
-) => () => {
-  dispatch(assignReviewer.request(captionerId))
-    .then(() => {
-      message.success(i18n.t("profile.roleAssignmentSuccess"));
-    })
-    .catch((error) => {
-      message.error(i18n.t("profile.roleAssignmentFailure", { error: error }));
-    });
-};
+export const handleAssignReviewer =
+  (captionerId: string, dispatch: Dispatch<any>) => () => {
+    dispatch(assignReviewer.request(captionerId))
+      .then(() => {
+        message.success(i18n?.t("profile.roleAssignmentSuccess"));
+      })
+      .catch((error) => {
+        message.error(
+          i18n?.t("profile.roleAssignmentFailure", { error: error })
+        );
+      });
+  };
 
-export const handleVerifyCaptioner = (
-  captionerId: string,
-  dispatch: Dispatch<any>
-) => () => {
-  dispatch(verifyCaptioner.request(captionerId))
-    .then(() => {
-      message.success(i18n.t("profile.roleAssignmentSuccess"));
-    })
-    .catch((error) => {
-      message.error(i18n.t("profile.roleAssignmentFailure", { error: error }));
-    });
-};
+export const handleVerifyCaptioner =
+  (captionerId: string, dispatch: Dispatch<any>) => () => {
+    dispatch(verifyCaptioner.request(captionerId))
+      .then(() => {
+        message.success(i18n?.t("profile.roleAssignmentSuccess"));
+      })
+      .catch((error) => {
+        message.error(
+          i18n?.t("profile.roleAssignmentFailure", { error: error })
+        );
+      });
+  };
 
-export const handleBanCaptioner = (
-  captionerId: string,
-  dispatch: Dispatch<any>
-) => () => {
-  dispatch(banCaptioner.request(captionerId))
-    .then(() => {
-      message.success(i18n.t("profile.roleAssignmentSuccess"));
-    })
-    .catch((error) => {
-      message.error(i18n.t("profile.roleAssignmentFailure", { error: error }));
-    });
-};
+export const handleBanCaptioner =
+  (captionerId: string, dispatch: Dispatch<any>) => () => {
+    dispatch(banCaptioner.request(captionerId))
+      .then(() => {
+        message.success(i18n?.t("profile.roleAssignmentSuccess"));
+      })
+      .catch((error) => {
+        message.error(
+          i18n?.t("profile.roleAssignmentFailure", { error: error })
+        );
+      });
+  };
