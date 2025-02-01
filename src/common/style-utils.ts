713c82540ab5435d183ba50a832e15e31caa7402
nopol10 <bobnewbie@gmail.com>
2025-02-01 22:37:47 +0800

    style: format style-utils

--- src/common/style-utils.ts
+++ src/common/style-utils.ts
@@ -4,10 +4,10 @@ import styled, { ThemedStyledFunction } from "styled-components";
 // Use withConfig directly instead
 export const styledNoPass = <
   O extends Record<string, unknown>,
-  C extends keyof JSX.IntrinsicElements | React.ComponentType<any> = "div"
+  C extends keyof JSX.IntrinsicElements | React.ComponentType<any> = "div",
 >(
   type: C,
-  displayName: string
+  displayName: string,
 ): ThemedStyledFunction<C, any, O, never> => {
   return styled(type).withConfig<O>({
     shouldForwardProp: (prop, defPropValFn) => defPropValFn(prop),
